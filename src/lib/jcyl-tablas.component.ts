b6c6ff2dae71859f2ae8d31cb8e5cdb64a5c64b3
David Parro Rubio <davidparro@gmail.es>
2019-05-28 11:54:45 +0200

    Debug onchanges

--- src/lib/jcyl-tablas.component.ts
+++ src/lib/jcyl-tablas.component.ts
@@ -45,6 +45,7 @@ export class JcylTablasComponent implements OnInit, OnChanges {
 
     ngOnChanges() {
         setTimeout(() => {
+            console.log('Cambiando la tabla');
             this.config.rows.forEach((row, i) => {
                 row.idTemp = i + 1;
             });
