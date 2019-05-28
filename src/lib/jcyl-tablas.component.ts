34716946b723e4727a74f8c7f92f805c084cd09d
David Parro Rubio <davidparro@gmail.es>
2019-05-28 11:49:46 +0200

    Añadido onchanges

--- src/lib/jcyl-tablas.component.ts
+++ src/lib/jcyl-tablas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
 import { Tabla, Field, Row } from './jcyl-tablas-models';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
     templateUrl: './jcyl-tablas.component.html',
     styleUrls: ['./jcyl-tablas.component.scss']
 })
-export class JcylTablasComponent implements OnInit {
+export class JcylTablasComponent implements OnInit, OnChanges {
 
     @Input() config: Tabla;
     @Output() clicBoton: EventEmitter<any> = new EventEmitter();
@@ -37,7 +37,14 @@ export class JcylTablasComponent implements OnInit {
         }
         this.rowsSelected = [];
         setTimeout(() => {
-            console.log(this.config.rows);
+            this.config.rows.forEach((row, i) => {
+                row.idTemp = i + 1;
+            });
+        });
+    }
+
+    ngOnChanges() {
+        setTimeout(() => {
             this.config.rows.forEach((row, i) => {
                 row.idTemp = i + 1;
             });
